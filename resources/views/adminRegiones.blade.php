77dcef5600767f3fa4cf4bc18c0dec20d3e351a2
Marcos Pinardi <exegeses@gmail.com>
2020-03-03 19:52:57 -0300

    adminRegiones

--- resources/views/adminRegiones.blade.php
+++ resources/views/adminRegiones.blade.php
@@ -1,12 +1,40 @@
 @extends('layouts.plantilla')
 
-    @section('titulo', 'título de la página')
+    @section('titulo', 'Panel de administración de regiones')
 
-    @section('h1', 'Tema  de la página')
+    @section('h1', 'Panel de administración de regiones')
 
     @section('main')
 
-        expeliarmus
+        <table class="table table-bordered table-striped table-hover">
+            <thead class="thead-dark">
+            <tr>
+                <th>id</th>
+                <th>Región</th>
+                <th colspan="2">
+                    <a href="" class="btn btn-dark">
+                        agregar
+                    </a>
+                </th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>id</td>
+                <td>Región</td>
+                <td>
+                    <a href="" class="btn btn-outline-secondary">
+                        modificar
+                    </a>
+                </td>
+                <td>
+                    <a href="" class="btn btn-outline-secondary">
+                        modificar 
+                    </a>
+                </td>
+            </tr>
+            </tbody>
+        </table>
 
    @endsection
 
