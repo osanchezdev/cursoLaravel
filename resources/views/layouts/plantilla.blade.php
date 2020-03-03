a1b8117ffe83bb262ddf8b519f1943aa8e26c590
Marcos Pinardi <exegeses@gmail.com>
2020-03-03 19:48:41 -0300

    plantilla

--- resources/views/layouts/plantilla.blade.php
+++ resources/views/layouts/plantilla.blade.php
@@ -21,10 +21,13 @@
             <div class="collapse navbar-collapse" id="navbarCollapse">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item active">
-                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
+                        <a class="nav-link" href="/adminRegiones">Admin Regiones</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/adminDestinos">Admin Destinos</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
